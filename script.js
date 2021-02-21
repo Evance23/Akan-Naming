4ae6cc01b1a6dab72c839d5caf6544b770178d4f
Evance23 <evancebarracks01@gmail.com>
2021-02-21 21:51:17 +0300

    Added js code to external script file

--- script.js
+++ script.js
@@ -1,2 +1,58 @@
-log.console ("It Aint");
-let outpu
+function myFunction() {
+    var gender = document.getElementById('gender').value;
+    var date = document.getElementById('date').value;
+
+    if (gender === 'male') {
+
+        var d = new Date(date);
+        var s = d.getDay();
+
+        if (s == 0 ) {
+            alert('Kwasi');
+        }
+        else if (s == 1 ) {
+            alert('Kwadwo');
+        }
+        else if (s == 2 ) {
+            alert('Kwabena');
+        }
+        else if (s == 3 ) {
+            alert('Kwaku');
+        }
+        else if (s == 4 ) {
+            alert('Yaw');
+        }
+        else if (s == 5 ) {
+            alert('Kofi');
+        }
+        else if (s == 6 ) {
+            alert('Kwame');
+        }
+    } else {
+        
+        var d = new Date(date);
+        var s = d.getDay();
+
+        if (s == 0 ) {
+            alert('Akosua');
+        }
+        else if (s == 1 ) {
+            alert('Adwowa');
+        }
+        else if (s == 2 ) {
+            alert('Abenaa');
+        }
+        else if (s == 3 ) {
+            alert('Akua');
+        }
+        else if (s == 4 ) {
+            alert('Yaa');
+        }
+        else if (s == 5 ) {
+            alert('Afua');
+        }
+        else if (s == 6 ) {
+            alert('Ama');
+        }
+    }
+}
